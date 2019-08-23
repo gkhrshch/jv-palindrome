05c59dba679e8fd16f29d4eb236de27aa4910e7e
ghkhrshch <george.khruschov@gmail.com>
2019-08-23 12:59:19 +0300

    Solution added

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class Palindrome {
 
     /**
-     * <p>Реализуйте метод, проверяющий, является ли заданная строка палиндромом. Палиндромом называется
-     * строка, которая читается одинаково слева направо и справа налево (в том числе пустая).
+     * <p>Реализуйте метод, проверяющий, является ли заданная строка палиндромом. Палиндромом назы-
+     * вается строка, которая читается одинаково слева направо и справа налево (в том числе пустая).
      * При определении "палиндромности" строки должны учитываться только буквы и цифры. А пробелы,
      * знаки препинания, а также регистр символов должны игнорироваться.</p>
      *
@@ -20,6 +20,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        String toCheck = text.toLowerCase().replaceAll("[^0-9a-z]", "");
+        StringBuilder toReverse = new StringBuilder(toCheck);
+        return toCheck.equals(toReverse.reverse().toString());
     }
 }
